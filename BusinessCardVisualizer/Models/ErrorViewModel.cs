b10446c0d33b86c52ca69b74d166fb186da3bbae
PetyaYakimova <petya_yakimova@abv.bg>
2024-01-19 15:03:00 +0200

    Add project files.

---
+++ BusinessCardVisualizer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BusinessCardVisualizer.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
